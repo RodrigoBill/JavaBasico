6f79df7f3a97c4a228e4a528a46df205396118fa
RodrigoBill <ramosrodrigo7@hotmail.com>
2017-05-22 22:46:21 -0300

    Primeiro Commit - Exercicio Looping and Arrays

---
+++ JavaBasico/src/OperadoresAritimeticos.java
@@ -0,0 +1,37 @@
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		
+		int resultado = 2 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado * 3;
+		System.out.println(resultado);
+		
+		resultado = resultado - 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado % 2;
+		System.out.println(resultado);
+
+		String primeiro = "Primeiro";
+		String segundo = "Segundo";
+		System.out.println("Concatenar o " + primeiro + " + " + segundo);
+		
+		resultado = 0;
+		
+		resultado++;
+		System.out.println(resultado);
+		++resultado;
+		System.out.println(resultado);
+		resultado--;
+		System.out.println(resultado);
+		--resultado;
+		System.out.println(resultado);
+	}
+
+}
